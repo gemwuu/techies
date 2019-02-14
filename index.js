0d69ba16537f47fbcb79144ac06e4c1697da4c7b
tianding.wk <tianding.wk@antfin.com>
2019-02-14 23:00:26 +0800

    feat: first version

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+/**
+ * @author: tianding.wk
+ * @createdTime: 2019-02-11 20:56:19
+ * @fileName: index.js
+ * @description: entry file
+ **/
+
+// module.exports = exports = require('./src/dom.js');
+import traverse from './src/dom';
+
+traverse(document.body);
